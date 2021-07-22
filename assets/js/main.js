2868fd6e5e8fa73d93397c1c3c6bd932cc62d588
abhaygupt <abhayrobotics1908@gmail.com>
2021-07-22 23:56:54 +0530

    Navbar Successfully running

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,31 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 
 
+
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 
+let navigation = document.getElementById('navigation')
+let app =document.getElementById('app')
+let closeBtn =document.getElementById('closeBtn')
 
+// **************************************show Menu
+function openMenu(){
+    
+    navigation.classList.add('active_menu')
+    
+    app.style.display ="none"
+    closeBtn.style.display ="inline-block"
+}
+
+// *****************************************Hide Menu
+function closeMenu(){
+    
+    navigation.classList.remove('active_menu')
+
+    app.style.display ="inline-block"
+    closeBtn.style.display ="none"
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
