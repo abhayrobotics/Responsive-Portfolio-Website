e5442256e15832008d548677e5dcff0cce799341
abhaygupt <abhayrobotics1908@gmail.com>
2021-07-24 13:43:31 +0530

    Skills design complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,70 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
 
 
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+// * **************************************Menu bar show**************
+
 
 let navigation = document.getElementById('navigation')
-let app =document.getElementById('app')
-let closeBtn =document.getElementById('closeBtn')
+let app = document.getElementById('app')
+let closeBtn = document.getElementById('closeBtn')
 
-// **************************************show Menu
-function openMenu(){
-    
+// *********************show Menu
+function openMenu() {
+
     navigation.classList.add('active_menu')
-    
-    app.style.display ="none"
-    closeBtn.style.display ="inline-block"
+
+    app.style.display = "none"
+    closeBtn.style.display = "inline-block"
 }
 
-// *****************************************Hide Menu
-function closeMenu(){
-    
+// *******************Hide Menu
+function closeMenu() {
+
     navigation.classList.remove('active_menu')
 
-    app.style.display ="inline-block"
-    closeBtn.style.display ="none"
+    app.style.display = "inline-block"
+    closeBtn.style.display = "none"
 }
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 
 
-/*==================== REMOVE MENU MOBILE ====================*/
+// ***************************skills show******************
+
+// frontend
+
+function frontend_skills() {
+
+    let skill_score1 = document.getElementById('skill-score1')
+    let arrow_down = document.getElementById('down')
+    let arrow_up = document.getElementById('up')
+
+    skill_score1.classList.toggle('active-skill');
+    arrow_down.classList.toggle('active-arrow')
+    arrow_up.classList.toggle('active-arrow')
+
+}
+
+// backend skills
 
 
-/*==================== ACCORDION SKILLS ====================*/
+function backend_skills() {
+    let skill_score = document.getElementById('skill-score2');
+    let arrow_up = document.getElementById('up2');
+    let arrow_down = document.getElementById('down2');
 
+    skill_score.classList.toggle('active-skill');
+    arrow_down.classList.toggle('active-arrow')
+    arrow_up.classList.toggle('active-arrow')
 
-/*==================== QUALIFICATION TABS ====================*/
+}
 
+// design skills
+function design_skills() {
+    let skill_score = document.getElementById('skill-score3');
+    let arrow_up = document.getElementById('up3');
+    let arrow_down = document.getElementById('down3');
 
-/*==================== SERVICES MODAL ====================*/
+    skill_score.classList.toggle('active-skill');
+    arrow_down.classList.toggle('active-arrow')
+    arrow_up.classList.toggle('active-arrow')
 
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+}
